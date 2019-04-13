46ec2ec49a5945a6fd0fe109e76d004d723601e9
Urbanz91 <turbino4ka91@gmail.com>
2019-04-13 19:03:37 +0300

    добавлено: хинт при отсутствии записей снов, импорт MatCardModule, стили карточки и хинта

--- src/app/components/angular-material/main.ts
+++ src/app/components/angular-material/main.ts
@@ -2,10 +2,12 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 export const AngularMaterialModule = [
   MatSidenavModule,
   MatListModule,
   MatToolbarModule,
   MatButtonModule,
+  MatCardModule,
 ];
