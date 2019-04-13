89c4d16ececc1d4e69c78663d82f4cb8a580ed70
Urbanz91 <turbino4ka91@gmail.com>
2019-04-13 15:50:49 +0300

    добавлено: 3 ссылки в меню, стили ссылок, импорт MatListModule

--- src/app/components/angular-material/main.ts
+++ src/app/components/angular-material/main.ts
@@ -1,5 +1,7 @@
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 export const AngularMaterial = [
-  MatSidenavModule
+  MatSidenavModule,
+  MatListModule
 ];
