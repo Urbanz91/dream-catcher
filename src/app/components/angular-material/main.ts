7c3eee7e4e3af9ef03f4e213acd79e27d49327e6
Urbanz91 <turbino4ka91@gmail.com>
2019-04-13 18:36:06 +0300

    добавлено: тулбар с кнопками, импорт MatToolbarModule, MatButtonModule, стили элементов

--- src/app/components/angular-material/main.ts
+++ src/app/components/angular-material/main.ts
@@ -1,7 +1,11 @@
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 export const AngularMaterialModule = [
   MatSidenavModule,
   MatListModule,
+  MatToolbarModule,
+  MatButtonModule,
 ];
