076c39955f2c4d738da49e99cd6a1ae0657d2145
Urbanz91 <turbino4ka91@gmail.com>
2019-06-10 22:49:55 +0300

    добавлено: -  компонент signup, login -  добавлен proxy для локального запуска -  библиотека ngrx -  поменян префикс с app на dc(dream-catcher) -  lazy loading для компонентов login и signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
 
 import { MyDreamsComponent } from './content/dream-catcher/main-block/my-dreams/my-dreams.component';
 import { DreamsPatternComponent } from './content/dream-catcher/main-block/dreams-pattern/dreams-pattern.component';
 import { DreamsStatisticComponent } from './content/dream-catcher/main-block/dreams-statistic/dreams-statistic.component';
 
-const routes: Routes = [
+const routes: Route[] = [
+  {
+    path: 'login',
+    loadChildren: () => import('./content/auth/login/login-routing.module').then(mod => mod.LoginRoutingModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./content/auth/signup/signup-routing.module').then(mod => mod.SignupRoutingModule)
+  },
   { path: '', redirectTo: '/my-dreams', pathMatch: 'full' },
   { path: 'my-dreams', component: MyDreamsComponent },
   { path: 'dreams-pattern', component: DreamsPatternComponent },
